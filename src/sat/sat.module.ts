58a02291be57cd67e7deefd5d0ebb8fbef187de3
João Pedro Pizoli Carvalho <jcarvalho@sou.unifeob.edu.br>
2026-02-18 12:52:03 -0300

    Fix: Adiciona import do MailModule no SatModule

--- src/sat/sat.module.ts
+++ src/sat/sat.module.ts
@@ -5,12 +5,14 @@ import { SatController } from "./sat.controller";
 import { SatService } from "./sat.service";
 import { AvtModule } from "src/avt/avt.module";
 import { UsuarioModule } from "src/usuario/usuario.module";
+import { MailModule } from "src/mail/mail.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([SatEntity]),
         AvtModule,
         UsuarioModule,
+        MailModule,
     ],
     controllers: [SatController],
     providers: [SatService],
