f536c6816908427202138c9c249b113136bebbc0
João Pedro Pizoli Carvalho <jcarvalho@sou.unifeob.edu.br>
2026-02-20 17:09:28 -0300

    Minor fix

--- src/shared/erp/erp.service.ts
+++ src/shared/erp/erp.service.ts
@@ -56,7 +56,7 @@ export class ErpService {
                     (DESCRICAO_ITEM LIKE ? OR CODIGO_ITEM LIKE ?) 
                     AND DEPARTAMENTO = 'PRODUTO ACABADO'
                     AND CODIGO_EMPRESA = 001
-                    AND DESCRICAO_ITEM NOT LIKE '%(ÑUSAR+)'
+                    AND DESCRICAO_ITEM NOT LIKE '%(ÑUSAR+)%'
                     `,
                 [`%${busca}%`, `%${busca}%`],
             );
